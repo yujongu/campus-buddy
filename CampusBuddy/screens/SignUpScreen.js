b3a720e348e8bccf407508c983f04dcc2bd547cf
Lucy Cheng <cheng448@purdue.edu>
2023-02-06 17:56:12 -0500

    sign up screen setup

---
+++ CampusBuddy/screens/SignUpScreen.js
@@ -0,0 +1,278 @@
+// Import React and Component
+import React, {useState, createRef} from 'react';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  Image,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+
+export default function SignUpScreen({ navigation, route })  {
+  const [userId, setUserId] = useState('');  
+  const [userFirstName, setUserFirstName] = useState('');
+  const [userLastName, setUserLastName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [errortext, setErrortext] = useState('');
+  const [
+    isRegistraionSuccess,
+    setIsRegistraionSuccess
+  ] = useState(false);
+
+  const firstNameInputRef = createRef();
+  const lastNameInputRef = createRef();
+  const emailInputRef = createRef();
+  const passwordInputRef = createRef();
+
+  const handleSubmitButton = () => {
+    setErrortext('');
+    if (!userId) {
+      alert('Please fill Username');
+      return;
+    }
+    if (!userFirstName) {
+        alert('Please fill First Name');
+        return;
+      }
+    if (!userLastName) {
+        alert('Please fill Last Name');
+        return;
+    }
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+    //Show Loader
+    setLoading(true);
+    var dataToSend = {
+      name: userName,
+      email: userEmail,
+      age: userAge,
+      address: userAddress,
+      password: userPassword,
+    };
+    var formBody = [];
+    for (var key in dataToSend) {
+      var encodedKey = encodeURIComponent(key);
+      var encodedValue = encodeURIComponent(dataToSend[key]);
+      formBody.push(encodedKey + '=' + encodedValue);
+    }
+    formBody = formBody.join('&');
+
+    fetch('http://localhost:3000/api/user/register', {
+      method: 'POST',
+      body: formBody,
+      headers: {
+        //Header Defination
+        'Content-Type':
+        'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //Hide Loader
+        setLoading(false);
+        console.log(responseJson);
+        // If server response message same as Data Matched
+        if (responseJson.status === 'success') {
+          setIsRegistraionSuccess(true);
+          console.log(
+            'Registration Successful. Please Login to proceed'
+          );
+        } else {
+          setErrortext(responseJson.msg);
+        }
+      })
+      .catch((error) => {
+        //Hide Loader
+        setLoading(false);
+        console.error(error);
+      });
+  };
+  if (isRegistraionSuccess) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#FEF9F0',
+          justifyContent: 'center',
+        }}>
+        <Text style={styles.successTextStyle}>
+          Registration Successful
+        </Text>
+      </View>
+    );
+  }
+  return (
+    <View style={{flex: 1, backgroundColor: '#FEF9F0'}}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}>
+        <KeyboardAvoidingView enabled>
+            <View style={{alignItems: 'center'}}>
+                <Image
+                source={require('../assets/campus-buddy.png')}
+                style={{
+                    width: '80%',
+                    height: 200,
+                    resizeMode: 'contain',
+                    margin: 10,
+                }}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(Username) => setUserId(Username)}
+              underlineColorAndroid="#f000"
+              placeholder="Enter Username"
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="sentences"
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                firstNameInputRef.current && firstNameInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserFirstName) => setUserName(UserFirstName)}
+              underlineColorAndroid="#f000"
+              placeholder="Enter First Name"
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="sentences"
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                lastNameInputRef.current && lastNameInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserLastName) => setUserLastName(UserLastName)}
+              underlineColorAndroid="#f000"
+              placeholder="Enter Last Name"
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="sentences"
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                emailInputRef.current && emailInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserEmail) => setUserEmail(UserEmail)}
+              underlineColorAndroid="#f000"
+              placeholder="Enter Email"
+              placeholderTextColor="#8b9cb5"
+              keyboardType="email-address"
+              ref={emailInputRef}
+              returnKeyType="next"
+              onSubmitEditing={() =>
+                passwordInputRef.current &&
+                passwordInputRef.current.focus()
+              }
+              blurOnSubmit={false}
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserPassword) =>
+                setUserPassword(UserPassword)
+              }
+              underlineColorAndroid="#f000"
+              placeholder="Enter Password"
+              placeholderTextColor="#8b9cb5"
+              ref={passwordInputRef}
+              returnKeyType="next"
+              secureTextEntry={true}
+              onSubmitEditing={Keyboard.dismiss}
+              blurOnSubmit={false}
+            />
+          </View>
+          {errortext != '' ? (
+            <Text style={styles.errorTextStyle}>
+              {errortext}
+            </Text>
+          ) : null}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            activeOpacity={0.5}
+            onPress={handleSubmitButton}>
+            <Text style={styles.buttonTextStyle}>REGISTER</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    SectionStyle: {
+      flexDirection: 'row',
+      height: 40,
+      marginTop: 20,
+      marginLeft: 35,
+      marginRight: 35,
+      margin: 10,
+    },
+    buttonStyle: {
+      backgroundColor: '#477A74',
+      borderWidth: 0,
+      color: '#FFFFFF',
+      borderColor: '#2F4858',
+      height: 40,
+      alignItems: 'center',
+      borderRadius: 30,
+      marginLeft: 35,
+      marginRight: 35,
+      marginTop: 20,
+      marginBottom: 20,
+    },
+    buttonTextStyle: {
+      color: '#FFFFFF',
+      paddingVertical: 10,
+      fontSize: 16,
+    },
+    inputStyle: {
+      flex: 1,
+      color: 'black',
+      paddingLeft: 15,
+      paddingRight: 15,
+      borderWidth: 1,
+      borderRadius: 30,
+      borderColor: '#2F4858',
+    },
+    errorTextStyle: {
+      color: 'red',
+      textAlign: 'center',
+      fontSize: 14,
+    },
+    successTextStyle: {
+      color: 'white',
+      textAlign: 'center',
+      fontSize: 18,
+      padding: 30,
+    },
+  });
