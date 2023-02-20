7a7744b0ac6f8791737c3a17a676b4c981e2071b
Ji Woong Park <park835@purdue.edu>
2023-02-20 02:51:59 -0500

    Added friend list button
    Click the button will show the real-time friend with unfriend button

--- CampusBuddy/screens/ProfileScreen.js
+++ CampusBuddy/screens/ProfileScreen.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import { auth, db } from "../firebaseConfig"
+import { Button, StyleSheet, Text, TextInput, View, Modal, TouchableOpacity, FlatList, ActivityIndicator } from "react-native";
+import { auth, db, userSchedule } from "../firebaseConfig"
 import { signOut } from "firebase/auth"
 import { updateDoc, doc } from "firebase/firestore";
 import { useState, useEffect } from "react";
@@ -9,6 +9,9 @@ import { onSnapshot } from "firebase/firestore";
 export default function ProfileScreen({ navigation, route }) {
   const [newId, setNewId] = useState("");
   const [id, setId] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const handleSignOut = () => {
     signOut(auth)
@@ -16,6 +19,8 @@ export default function ProfileScreen({ navigation, route }) {
         navigation.popToTop();
       })
   }
+
+  
 
   const handleChangeId = () => {
     const userDocRef = doc(db, "users", auth.currentUser.uid);
@@ -29,6 +34,10 @@ export default function ProfileScreen({ navigation, route }) {
   }
 
   useEffect(() => {
+    const subscriber = onSnapshot(doc(db, "friend_list", auth.currentUser?.email), (doc) => {
+      setList(doc.data()['friends'])
+      setLoading(false)
+    })
     const userDocRef = doc(db, "users", auth.currentUser.uid);
     const unsubscribe = onSnapshot(userDocRef, (doc) => {
       if (doc.exists()) {
@@ -42,8 +51,52 @@ export default function ProfileScreen({ navigation, route }) {
     };
   }, []);
 
+  const renderItem = (item) =>{
+    return (
+      <View style={styles.item}>
+        <Text style={{color: 'black', fontSize: 15}}>{item}</Text>
+        <View style={{flexDirection: "row"}}>
+          <TouchableOpacity onPress={() => alert("Unfriended")}>
+            <Text>Unfriend</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
+      <Modal
+          animationType="slide"
+          transparent={true}
+          visible={visible}
+          onRequestClose={() => {
+            setVisible(!visible)
+          }}
+        >
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View style={{width: '70%', height:'70%', backgroundColor: 'blue'}}>
+              {
+                loading ?
+                <ActivityIndicator />
+                :
+                <FlatList 
+                  data={list}
+                  renderItem={({item}) => 
+                    renderItem(item)
+                  }
+                />
+              }
+            </View>
+          </View>
+      </Modal>
       <Text>{auth.currentUser?.uid}</Text>
       <TextInput
         style={styles.input}
@@ -54,6 +107,7 @@ export default function ProfileScreen({ navigation, route }) {
       <Text>Current Id: {id}</Text>
       <Button title="Change Id" onPress={handleChangeId} />
       <Button title="Sign Out" onPress={handleSignOut} />
+      <Button title="Friend list" onPress={() => setVisible(true)} />
     </View>
   );
 }
@@ -73,5 +127,15 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginVertical: 10,
     padding: 10,
+  },
+  item: {
+    backgroundColor: 'orange',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    width: '90%',
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   },
 });
