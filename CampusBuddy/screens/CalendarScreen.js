18aa53f0ee97724e5ea040b217de1a78799f7511
Jongu Yu <yujongu@gmail.com>
2023-03-06 19:54:53 -0500

    fix holiday setting modal UI

--- CampusBuddy/screens/CalendarScreen.js
+++ CampusBuddy/screens/CalendarScreen.js
@@ -980,6 +980,7 @@ export default class App extends Component {
                   justifyContent: "space-between",
                   marginLeft: 10,
                   marginRight: 10,
+                  marginBottom: 10,
                 }}
               >
                 <Button
