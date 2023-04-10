7d050552af5e3dfd86c1e772b8f70052acb8c605
Lucy Cheng <cheng448@purdue.edu>
2023-04-10 19:45:31 -0400

    goals modal format

--- CampusBuddy/screens/GoalsScreen.js
+++ CampusBuddy/screens/GoalsScreen.js
@@ -22,19 +22,34 @@ import theme from "../components/ui/theme";
 
 export default function ProfileScreen({ navigation, route }) {
   const [visible, setVisible] = useState(false);
+  const [pointGoal, setPointGoal] = useState(0);
+  const [category, setCategory] = useState("");
+  const [deadlineDate, setDeadlineDate] = useState(null);
+  const [deadlineTime, setDeadlineTime] = useState(null);
+
 
   return (
     <View style={[styles.container]}>
         <View style= {styles.row}>
-        <Text style={{paddingRight:10}}>My Goals</Text>
+        <Text style={{paddingRight:10, fontSize:20}}>My Goals</Text>
         <TouchableOpacity
+            style = {{left:10, bottom:2}}
             onPress={() =>
             setVisible(true)
             }
         >
-            <Icon name="plus" size={20} color="#2F4858" />
+            <Icon name="plus" size={25} color="#2F4858" />
         </TouchableOpacity>
         </View>
+        <View style= {{top:30}}>
+        <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={()=>navigation.goBack()}
+        >
+            <Icon name="mail-reply" size={20} color="#2F4858"  />
+        </TouchableOpacity>
+        </View>
+        
         <Modal
           animationType="fade"
           visible={visible}
@@ -52,8 +67,6 @@ export default function ProfileScreen({ navigation, route }) {
               style={{
                 backgroundColor: "white",
                 marginHorizontal: 20,
-                height: "40%",
-                width:"80%",
                 borderRadius: 10,
               }}
             >
@@ -61,6 +74,7 @@ export default function ProfileScreen({ navigation, route }) {
                   style={{
                     flexDirection: "column",
                     alignItems: "center",
+                    margin:15
                   }}
                 >
                   <View style={styles.row}>
@@ -80,6 +94,69 @@ export default function ProfileScreen({ navigation, route }) {
                   <View style={styles.row}>
                     <Text style={styles.header_text}>New Goal</Text>
                   </View>
+                  <View style={styles.row}>
+                  <Text
+                      style={{
+                        textAlign: "center",
+                        color: "#2F4858",
+                        marginTop:15,
+                      }}
+                    >
+                      Category:
+                    </Text>
+                  <TextInput
+                       style={{
+                        color: "black",
+                        borderWidth: 1,
+                        borderColor: "#8b9cb5",
+                        margin:10,
+                        width: 100,
+                        height: 30,
+                        textAlign: "center",
+                      }}
+                      placeholderTextColor="#8b9cb5"
+                      onChangeText={(text) => this.setTitle(text)}
+                    ></TextInput>
+                    </View>
+                  <View style={styles.row}>
+                  <Text
+                      style={{
+                        textAlign: "center",
+                        color: "#2F4858",
+                        marginTop:15,
+                      }}
+                    >
+                      Point Goal:
+                    </Text>
+                    <TextInput
+                      placeholderTextColor="#8b9cb5"
+                      style={{
+                        color: "black",
+                        borderWidth: 1,
+                        borderColor: "#8b9cb5",
+                        margin:10,
+                        width: 50,
+                        height: 30,
+                        textAlign: "center",
+                      }}
+                      value={pointGoal}
+                      defaultValue={0}
+                      keyboardType="numeric"
+                      onChangeText={(text) => setPointGoal(text)}
+                    />
+                    </View>
+                    <View style={styles.row}>
+                  <Text
+                      style={{
+                        textAlign: "center",
+                        color: "#2F4858",
+                        margin:15,
+                      }}
+                    >
+                      Deadline:
+                    </Text>
+                    </View>
+
                   <Button
                     title="Set Goal"
                     onPress={() => {
@@ -98,8 +175,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
+    marginTop: 100,
   },
   textStyle: {
     fontWeight: "bold",
@@ -108,5 +184,11 @@ const styles = StyleSheet.create({
   },
   row: {
     flexDirection: "row",
+  },
+  header_text: {
+    flex: 1,
+    fontSize: 20,
+    textAlign: "center",
+    paddingBottom:10
   },
 });
